029712320730670df0f1b7653ac5e6f9aaff7415
SidraOsman <osmansidra4@gmail.com>
2023-10-03 13:39:23 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code in this file!
+const currentUser ="Sidra Osman";
+const welcomeMessage = 'Welcome to Flatbook, ${Sidra Osman}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome, ${currentUser.slice(0, 2)}!`;
+
+
